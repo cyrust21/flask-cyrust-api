f46d311b8b0ec6008b11a67c57c054f26db6173c
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-21 19:52:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -111,5 +111,7 @@ def get_region_info():
 
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run()
+import os
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
