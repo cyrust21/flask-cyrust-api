de32b68279352efa83ec97a0d9e37a227a8ce5c9
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-23 15:05:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from rich import print as rprint
 from rich.progress import Progress
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/cyrust-region/*": {"origins": "https://vid2viral.blogspot.com"}})
 console = Console()
 
 def check_player_info(target_id):
